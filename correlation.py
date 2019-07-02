afd3e40b099193a8ec4e2caff7b21f590ec79837
Erick Rodriguez Salas <eers500@its.york.ac.uk>
2019-07-02 21:42:06 +0100

    Matplotlib with data points

--- correlation.py
+++ correlation.py
@@ -4,6 +4,7 @@ import time
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 import numpy as np
+import mpldatacursor
 from progress.bar import Bar
 
 ##
@@ -20,22 +21,32 @@ IBFTS = np.fft.fftshift(IBFT)
 R = IFT*np.conj(IBFT)
 r = np.real(np.fft.ifftshift(np.fft.ifft2(R)))
 
-##
-from bokeh.plotting import figure, show, output_file
+## Bokeh plot
+# from bokeh.plotting import figure, show, output_file
+#
+# p = figure(tooltips=[("x", "$x"), ("y", "$y"), ("value", "@image")])
+# # p.x_range.range_padding = p.y_range.range_padding = 0
+#
+# # must give a vector of image data for image parameter
+# im = r
+# p.image(image=[im[::-1]], x=0, y=0, dw=512, dh=512, palette="Spectral11")
+#
+# output_file("image.html", title="image.py example")
+#
+# show(p)  # open a browser
 
-p = figure(tooltips=[("x", "$x"), ("y", "$y"), ("value", "@image")])
-# p.x_range.range_padding = p.y_range.range_padding = 0
-
-# must give a vector of image data for image parameter
-im = r
-p.image(image=[im[::-1]], x=0, y=0, dw=512, dh=512, palette="Spectral11")
-
-output_file("image.html", title="image.py example")
-
-show(p)  # open a browser
-
-##
+## Pyplot plot
+plt.figure(1)
 plt.imshow(r, cmap='gray')
 plt.colorbar()
+mpldatacursor.datacursor(hover=True, bbox=dict(alpha=1, fc='w'),
+                         formatter='x, y = {i}, {j}\nz = {z:.02g}'.format)
+plt.show()
 
+## Seaborn plot
+# import seaborn as sns
+#
+# plt.figure(2)
+# sns.heatmap(r, cmap='gray')
+#
 
