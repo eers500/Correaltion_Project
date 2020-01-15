c8154c449b585579abda49dab5bd1c4c7a337606
Erick Rodriguez Salas <eers500@its.york.ac.uk>
2020-01-15 13:06:48 +0000

    Update figures.py

--- Optalysys/Ground Truth/figures.py
+++ Optalysys/Ground Truth/figures.py
@@ -1,13 +1,28 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+plt.style.use('default')
+
 P1 = np.loadtxt('particle1.txt')
 P2 = np.loadtxt('particle2.txt')
 P3 = np.loadtxt('particle3.txt')
 
-#%%
-plt.plot(P1[:, 0], P1[:, 0])
-plt.plot(P1[:, 0], P1[:, 3], 'rs')
-plt.xlabel('Plate Position [$\mu$m]')
-plt.ylabel('DZ [$\mu$m]')
+# %%
+fig, ax = plt.subplots(3, 2, sharex=True)
+
+ax[0, 0].plot(P1[:, 0], P1[:, 0], 'k-', linewidth=2)
+ax[0, 0].plot(P1[:, 0], P1[:, 3], 'bs', markersize=13, fillstyle='none', markeredgewidth=2)
+ax[0, 0].tick_params(axis='both', direction='in', which='both', top=True, right=True)
+
+ax[1, 0].plot(P2[:, 0], P2[:, 0], 'k-', linewidth=2)
+ax[1, 0].plot(P2[:, 0], P2[:, 3], 'ro', markersize=13, fillstyle='none', markeredgewidth=2)
+ax[1, 0].tick_params(axis='both', direction='in', which='both', top=True, right=True)
+
+ax[2, 0].plot(P3[:, 0], P3[:, 0], 'k-', linewidth=2)
+ax[2, 0].plot(P3[:, 0], P3[:, 3], 'g^', markersize=13, fillstyle='none', markeredgewidth=2)
+ax[2, 0].tick_params(axis='both', direction='in', which='both', top=True, right=True)
+
+ax[2, 0].set_xlabel('Z [$\mu$m]', fontsize=20)
+ax[1, 0].set_ylabel('Z\' [$\mu$m]', fontsize=20)
+
 plt.show()
